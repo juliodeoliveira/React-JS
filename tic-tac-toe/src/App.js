b0163717ecbafe4a4759bdf572aeb5e5a66a8d2c
Júlio de Oliviera <ramosju423@gmail.com>
2023-03-24 19:39:40 -0300

    Funções novas adicionadas

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,35 +1,40 @@
-function Square({value}) {
+import { useState } from "react"
 
-  function getClick() {
-    console.log("what u want!")
-  }
-  
+function Square({value, squareClicked}) { 
   return (
-    <button className="square" onClick={getClick}>{value}</button>
+    <button className="square" onClick={squareClicked}> {value} </button>
     
   )
 }
 
+
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  
+  function gotClicked(s) {
+    const nearSquares = squares.slice()
+    nearSquares[s] = "X"
+    setSquares(nearSquares)
+  }
 
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square value={squares[0]} squareClicked={() => gotClicked(0)}/>
+        <Square value={squares[1]} squareClicked={() => gotClicked(1)}/>
+        <Square value={squares[2]} squareClicked={() => gotClicked(2)}/>
       </div>
 
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square value={squares[3]} squareClicked={() => gotClicked(3)}/>
+        <Square value={squares[4]} squareClicked={() => gotClicked(4)}/>
+        <Square value={squares[5]} squareClicked={() => gotClicked(5)}/>
       </div>
 
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square value={squares[6]} squareClicked={() => gotClicked(6)}/>
+        <Square value={squares[7]} squareClicked={() => gotClicked(7)}/>
+        <Square value={squares[8]} squareClicked={() => gotClicked(8)}/>
       </div>
     </>
   )
